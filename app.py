4de346b2f7eeba5c5e4dc9fcea935f17ca33da79
Cynthia Sadziwa <sadziwacynthia@gmail.com>
2026-06-17 12:39:08 -0700

    Add timetable import and teacher fee visibility fixes

--- app.py
+++ app.py
@@ -6430,6 +6430,7 @@ if __name__ == "__main__":
     create_notices_table()
     create_assessments_table()
     add_class_teacher_column()
+    add_school_id_to_audit_logs()
     setup_app()
 
     port = int(os.environ.get("PORT", 10000))
